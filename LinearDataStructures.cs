556fd9fb8b88889aae541ac1742ead31bcd75a98
Paritosh Tiwari <Paritosh1.tiwari@gmail.com>
2023-01-08 13:39:42 -0600

    Changes for Concept

---
+++ LinearDataStructures.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingConcepts
+{
+    public class LinearDataStructures
+    {
+        /// <summary>
+        /// Queue Testing for the required methods
+        /// </summary>
+        public void QueueMethods()
+        {
+            NewRegionStarted("Queue");
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+            queue.Enqueue("d");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            queue.Dequeue();
+
+            Console.WriteLine("After Dequeue");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Peek " + queue.Peek());
+            var a = queue.ToArray();
+            Console.WriteLine("Array " + a[0]);
+        }
+
+        public void ArrayMethods()
+        {
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+            NewRegionStarted("Array");
+            Console.WriteLine("The first element of an array {0}", arr[0]);
+        }
+
+        public void StackMethods()
+        {
+            NewRegionStarted("Stack");
+            Stack<string> stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            stack.Push("d");
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Popping first Element of Stack");
+            stack.Pop();
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Peek {0}", stack.Peek());
+
+        }
+
+        public void NewRegionStarted(string region)
+        {
+            Console.WriteLine("------------------------------------{0} Started---------------------------", region);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using System;
+using System.Runtime.InteropServices;
+using TestingConcepts;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+
+public class Program
+{
+    public static void Main()
+    {
+        LinearDataStructures d = new LinearDataStructures();
+        #region Linear Data Structure
+
+        #region Static Linear Data Structure - Array
+        /// The arrays can be used in various places where we need to store the data of similar data type
+        d.ArrayMethods();
+        #endregion
+
+        #region Dynamic Linear Data Structure
+        #region Uses and Calling of Queue
+        /// To Test Queue
+        /// FCFS ( first come first serve) scheduling, example: FIFO queue
+        /// Spooling in printers
+        /// Buffer for devices like keyboard
+        /// CPU Scheduling
+        /// Memory management
+        /// Queues in routers/ switches 
+        /// Mail Queues
+        /// Variations: (Deque, Priority Queue, Doubly Ended Priority Queue )
+        /// Applied as waiting lists for a single shared resource like CPU, Disk, and Printer.
+        /// Applied as buffers on MP3 players and portable CD players.
+        /// Applied on Operating system to handle the interruption.
+        /// Applied to add song at the end or to play from the front.
+        /// Applied on WhatsApp when we send messages to our friends and they don’t have an internet connection then these messages are queued on the server of WhatsApp.
+        /// Traffic software(Each  light get on one by one after every time of interval of time.)
+        d.QueueMethods();
+        #endregion
+        #region Uses and Calling of Stack
+        d.StackMethods();
+        #endregion
+        #region Uses and Calling of LinkedList
+        #endregion
+        #endregion
+
+
+
+        #endregion
+    }
+}
